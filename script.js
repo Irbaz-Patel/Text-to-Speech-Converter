18c584c5aefd9e265a246f5f956212f3027730b8
Irbaz Patel <irbazahmed1711@gmail.com>
2023-10-17 22:38:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let speech=new SpeechSynthesisUtterance();
+
+let voice=[]
+
+let voiceSelect=document.querySelector("select")
+window.speechSynthesis.onvoiceschanged=()=>{
+    voice=window.speechSynthesis.getVoices();
+    speech.voice=voice[0]
+
+    voice.forEach((voice,i)=>(voiceSelect.options[i])=new Option(voice.name,i))
+};
+
+voiceSelect.addEventListener("change",()=>{
+    voice.speech=voice[voiceSelect.value];
+})
+
+document.querySelector("button").addEventListener("click",()=>{
+    speech.text=document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+})
+
